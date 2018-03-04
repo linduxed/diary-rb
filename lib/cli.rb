3408ad7688c748dc02112748b73cbff41c2a36aa
Mikael Muszynski <linduxed@gmail.com>
2018-03-04 03:49:41 +0100

    Read TODOs from today with todo.sh
    Insert done TODOs from the day of writing the day into the template.
    
    This patch relies on usage of the todo.sh utility for managing TODOs.
    The way the TODOs are extracted and cleaned up is very specific to how
    my todo.sh is configured and what add-ons I use.
    
    There might be a way to make this more more generic, making it easier
    for others to use this script, but this is currently not a concern of
    mine.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class CLI
   def run
     write_template(entry_path)
@@ -31,8 +33,18 @@ class CLI
   end
 
   def entry_template
-    <<~EOF
+    return @entry_template if @entry_template
 
+    done_todos = Todos.new.done_today
+    todo_lines = done_todos.map { |todo| "* #{todo}" }.join("\n")
+
+    @entry_template = <<~EOF
+      # Entry
+
+
+      ### Done tasks
+
+      #{todo_lines}
 
       ### Tags
 
@@ -52,5 +64,40 @@ class CLI
 
   def write_template(path)
     File.open(path, 'w') { |file| file.puts(entry_template) }
+  end
+end
+
+class Todos
+  TODO = Struct.new(:date_of_completion, :text, keyword_init: true) do
+    def to_s
+      text
+    end
+  end
+
+  def done_today
+    done_todos.select { |todo| todo.date_of_completion == Date.today }
+  end
+
+  private
+
+  def done_todos
+    all_todos_output = `todo.sh -p listall`
+    done_todo_lines = all_todos_output.lines(chomp: true).select do |line|
+      line.start_with?(/\d+ x /)
+    end
+
+    done_todo_lines.map do |todo_line|
+      line_without_number_and_completion_char = todo_line.gsub(/\A\d+ x /, '')
+      date_of_completion = line_without_number_and_completion_char.
+        split(' ').first
+
+      todo_without_dates = line_without_number_and_completion_char.
+        gsub(/\A(\d{4}-\d{2}-\d{2} ){2}(\{\d{4}.\d{2}.\d{2}\} )?/, '')
+
+      TODO.new(
+        date_of_completion: Date.parse(date_of_completion),
+        text: todo_without_dates
+      )
+    end
   end
 end
