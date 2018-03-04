ffbe148fa5ed715dd642ae1754d4ee3d00929bf1
Mikael Muszynski <linduxed@gmail.com>
2018-03-04 02:00:43 +0100

    Add first version of diary script
    The idea of the script is to write a template to a file and let the user
    edit that file.
    
    The script deletes the file if no changes were made, since this likely
    means that the user changed its mind about adding an entry.

---
+++ lib/cli.rb
@@ -0,0 +1,56 @@
+class CLI
+  def run
+    write_template(entry_path)
+
+    Kernel.system("#{editor} \"#{entry_path}\"")
+
+    delete_if_empty_entry
+    exit 0
+  end
+
+  private
+
+  def delete_if_empty_entry
+    entry_contents = File.read(entry_path)
+    if entry_contents == entry_template
+      $stdout.puts 'Empty entry: deleting'
+      File.delete(entry_path)
+    end
+  end
+
+  def editor
+    @editor ||= fetch_from_env!('EDITOR')
+  end
+
+  def entry_path
+    return @entry_path if @entry_path
+
+    diary_path = fetch_from_env!('DIARY_PATH')
+    date_and_time_str = Time.now.to_s.gsub(' ', '__')
+    @entry_path = "#{File.expand_path(diary_path)}/#{date_and_time_str}.md"
+  end
+
+  def entry_template
+    <<~EOF
+
+
+      ### Tags
+
+    EOF
+  end
+
+  def fetch_from_env!(var_name)
+    var = ENV[var_name]
+
+    unless var
+      $stdout.puts "#{var_name} must be set"
+      exit 1
+    end
+
+    var
+  end
+
+  def write_template(path)
+    File.open(path, 'w') { |file| file.puts(entry_template) }
+  end
+end
