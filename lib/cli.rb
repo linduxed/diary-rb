60ed57020bbfd31c84f49859185dfd1056b7382b
Mikael Muszynski <linduxed@gmail.com>
2018-09-06 14:37:25 +0200

    WIP - show day in pager

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,17 +4,33 @@ require 'tempfile'
 
 class CLI
   def run
-    write_template
+    if ARGV[0] == 'show' && ARGV[1]
+      date = parse_date(ARGV[1])
 
-    Kernel.system("#{editor} \"#{entry_path}\"")
+      show_day_in_pager(date)
+    else
+      write_template
 
-    remove_todo_list_at_bottom
-    delete_and_exit_if_empty_entry
+      Kernel.system("#{editor} \"#{entry_path}\"")
 
-    show_random_previous_day_in_pager
+      remove_todo_list_at_bottom
+      delete_and_exit_if_empty_entry
+
+      show_random_previous_day_in_pager
+    end
   end
 
   private
+
+  def parse_date(date)
+    if date == 'today'
+      Date.today
+    elsif date == 'yesterday'
+      Date.today - 1
+    else
+      Date.parse(date)
+    end
+  end
 
   def delete_and_exit_if_empty_entry
     if File.readlines(entry_path).all? { |line| line.match?(/\A\s*\z/) }
@@ -78,6 +94,64 @@ class CLI
 
   def diary_pager
     @pager ||= fetch_from_env!('DIARY_PAGER')
+  end
+
+  def show_day_in_pager(date)
+    diary_file_names = Dir.children(diary_path)
+    entry = Struct.new(:file_name, :diary_path) do
+      def entry_contents
+        File.read(full_entry_path).chomp
+      end
+
+      def full_entry_path
+        "#{diary_path}/#{file_name}"
+      end
+
+      def to_date
+        Date.parse(file_name.gsub('__', ' ').gsub(/\.md\z/, ''))
+      end
+
+      def to_datetime
+        DateTime.parse(file_name.gsub('__', ' ').gsub(/\.md\z/, ''))
+      end
+    end
+
+    entries = diary_file_names.map do |file_name|
+      entry.new(file_name, diary_path)
+    end
+
+    all_entries_from_date = entries.
+      select { |entry| entry.to_date == date }.
+      sort_by(&:to_datetime)
+
+    tempfile = Tempfile.new('diary')
+
+    formatted_day = date.strftime('%A, %F W%V')
+    tempfile.puts(formatted_day)
+    tempfile.puts('=' * formatted_day.length)
+    tempfile.puts
+
+    all_entries_from_date.each do |entry_from_date|
+      formatted_time = entry_from_date.to_datetime.strftime('%R')
+
+      tempfile.puts(formatted_time)
+      tempfile.puts('-' * formatted_time.length)
+      tempfile.puts
+      tempfile.puts(entry_from_date.entry_contents)
+      tempfile.puts
+    end
+
+    tempfile.puts('### Done tasks')
+    tempfile.puts
+    Todos.new.done_for(date).each do |todo|
+      tempfile.puts("* #{todo}")
+    end
+
+    tempfile.close
+
+    Kernel.system("#{diary_pager} #{tempfile.path}")
+
+    tempfile.delete
   end
 
   def show_random_previous_day_in_pager
